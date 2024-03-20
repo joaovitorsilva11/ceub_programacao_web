8fd1d8313498d0f7ffa70616b8377b42326c42a7
joaovitorsilva11 <joao.silva16@sempreceub.com>
2024-03-20 09:10:34 -0300

    criação do JS

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("teste")
+
+let teste = 1.1
+let variavelString = "Hello, World!";
+
+console.log(teste);
+console.log(variavelString);
+console.log(typeof(teste));
+
+//let a = "5";
+//let b = 2;
+
+//console.log(typeof(a));
+//console.log(typeof(b));
+
+//let numero1 = 0.1;
+//let numero2 = 0.2;
+
+let texto1 = "ola";
+let texto2 = "mundo";
+
+let resultadoTexto = texto1 + ", " + texto2 + "isadbnfiasbf " + " isadbfioabsdf";
+
+console.log(resultadoTexto);
+
+/*
+let resultado = a - b;
+let resultado2 = a + b;
+console.log(resultado);
+console.log(resultado2);
+console.log(typeof(resultado));
+console.log(typeof(resultado2));
+*/
